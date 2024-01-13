be92acef7f7b1c0281f742b81bee9e6c5a38d2b9
Jorge Rios <jorgeluisrios47@gmail.com>
2024-01-13 09:49:48 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const workoutRoutes = require("./routes/workouts.js");
+const mongoose = require("mongoose");
+const express = require("express");
+require("dotenv").config()
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// connect to mongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
